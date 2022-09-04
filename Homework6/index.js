e6d3118bb92809a096c193f7bebab72df8668fc1
KristinaFedorova <kristinka.fedorova.04@gmail.com>
2022-09-04 13:31:45 +0200

    redo 6 homework

--- Homework6/index.js
+++ Homework6/index.js
@@ -16,7 +16,9 @@ function clearAll() {
             element.classList.remove('highlight')
         }
     }
+
 }
+
 
 function clearInput() {
     input.value = null
@@ -26,67 +28,28 @@ function findElement(selector) {
     if (seekedElement[selector] === null) {
         return 'no find'
     }
+    clearAll()
     seekedElement = seekedElement[selector]
     seekedElement.classList.add('highlight')
-    clearAll()
+    console.log(seekedElement)
 
 }
 
-function toggleBtnStylePrev() {
+function toggleBtnStyle() {
     if (seekedElement.previousElementSibling) {
         previousBtn.disabled = false
         previousBtn.classList.add('active')
+
     } else {
         previousBtn.disabled = true
         previousBtn.classList.add('disabled')
     }
 }
 
-function toggleBtnStyleNext() {
-    if (seekedElement.nextElementSibling) {
-        nextBtn.disabled = false
-        nextBtn.classList.add('active')
-    } else {
-        nextBtn.disabled = true
-        nextBtn.classList.add('disabled')
-    }
-}
-
-function toggleBtnStyleParent() {
-    if (seekedElement.parentElement) {
-        parentBtn.disabled = false
-        parentBtn.classList.add('active')
-    } else {
-        parentBtn.disabled = true
-        parentBtn.classList.add('disabled')
-    }
-}
-
-function toggleBtnStyleFirstChild() {
-    if (seekedElement.firstElementChild) {
-        firstBtn.disabled = false
-        firstBtn.classList.add('active')
-    } else {
-        firstBtn.disabled = true
-        firstBtn.classList.add('disabled')
-    }
-}
-
-function toggleBtnStyleLastChild() {
-    if (seekedElement.lastElementChild) {
-        lastBtn.disabled = false
-        lastBtn.classList.add('active')
-    } else {
-        lastBtn.disabled = true
-        lastBtn.classList.add('disabled')
-    }
-}
-
-
 input.addEventListener('input', function () {
     seekedElement = document.querySelector(this.value)
     if (seekedElement) {
-        toggleBtnStylePrev()
+        toggleBtnStyle()
         clearAll()
         seekedElement.classList.add('highlight')
     }
@@ -96,23 +59,23 @@ clearBtn.addEventListener('click', clearInput)
 
 previousBtn.addEventListener('click', function () {
     findElement('previousElementSibling')
-    toggleBtnStylePrev()
+    toggleBtnStyle()
 })
 
 nextBtn.addEventListener('click', function () {
     findElement('nextElementSibling')
-    toggleBtnStyleNext()
+    toggleBtnStyle()
 })
 parentBtn.addEventListener('click', function () {
     findElement('parentElement')
-    toggleBtnStyleParent()
+    toggleBtnStyle()
 })
 firstBtn.addEventListener('click', function () {
     findElement('firstElementChild')
-    toggleBtnStyleFirstChild()
+    toggleBtnStyle()
 })
 lastBtn.addEventListener('click', function () {
     findElement('lastElementChild')
-    toggleBtnStyleLastChild()
+    toggleBtnStyle()
 })
 
