0830bdfd8e0bb35452cd9e259f5ba38bd0637079
KristinaFedorova <kristinka.fedorova.04@gmail.com>
2022-10-21 22:54:53 +0200

    16 homework

---
+++ my-app/src/components/api.js
@@ -0,0 +1,22 @@
+const getData = async () => {
+    try {
+        let response = await fetch("https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11")
+        let currencyData = await response.json();
+        return currencyData;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+const displayData = async () => {
+    try {
+        let result = await getData();
+        let [USD, EUR, ignore] = result;
+        return {'USD': USD, 'EUR': EUR}
+
+    } catch (e) {
+        console.error(e);
+    }
+}
+displayData();
+export default displayData;
